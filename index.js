8327498cbfe23a9fc94d71f82797c166a62342e1
Kevin Nwoke <kevinnwoke@yahoo.com>
2017-12-08 23:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name){
+function happyHolidaysTo(you){
   return "Happy holidays, ${name}!"
 }
-happyHolidaysTo(you)
 
 function happyHolidayTo(holiday, name){
   return "Happy ${holiday}, ${name}!"
