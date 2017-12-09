2d0541ec95654373381d21b5bce7026603ac72fc
Kevin Nwoke <kevinnwoke@yahoo.com>
2017-12-09 02:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function happyHolidaysTo(name){
 happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!"
+  return "Happy Independence Day, you"
 }
 happyHolidayTo('Independence Day','you')
 
 function holidayCountdown(holiday, days){
   var holiday = "Mother's Day"
-  return "It's ${days} until ${holiday}!"
+  return "It's 20 until Mother's Day!"
 }
 holidayCountdown("Mother's Day", 20)
